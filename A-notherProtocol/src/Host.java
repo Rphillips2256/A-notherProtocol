4c5dd52f16da1cba9209888bc995a3d6c018bf40
rs5634nr <rphillips2256@gmail.com>
2017-11-14 10:55:38 -0600

    newest

--- A-notherProtocol/src/Host.java
+++ A-notherProtocol/src/Host.java
@@ -20,11 +20,12 @@ public class Host {
 	public static void main(String [] args){
 		
 		String filename;
-		byte [] header;
-		byte [] mainData;
+		byte [] servAddress;
+		byte [] addr;
 		byte [] packetMessage = new byte[1024];
 		int desPri, gatewayPort, serverPort;
 		CRC32 checksum = new CRC32();
+		boolean open = false;
 		
 		
 		
@@ -38,7 +39,7 @@ public class Host {
 			
 			//Get the IG ip address and port number
 			//modify this to make it correct.
-			byte [] b = new byte[] {(byte) 146,(byte) 57,(byte) 194,(byte) 238};
+			byte[] b = new byte[] {(byte) 146,(byte) 57,(byte) 194,(byte) 238};
 			InetAddress address = null;
 			try {
 	        		address = InetAddress.getByAddress(b);
@@ -49,54 +50,66 @@ public class Host {
 			
 			
 			// Determine server port number
-			serverPort = 58989;
+			gatewayPort = 58989;
+			
+			desPri = 1;
 			
 			
-			//Build the header of the open connection packet
+			//Build the header and data of the open connection packet
 			
+			addr = clientSocket.getInetAddress().getAddress() ;
+			packetMessage = new byte[] {(byte) 146,(byte) 57,(byte) 194,(byte) 238,(byte) gatewayPort,
+					 (byte) clientSocket.getLocalPort()};
 			
-			
-			
-			
-			//build the open connection header
-			
-			
+			for(int i = 0; i < addr.length; i++){
+				packetMessage[i+6] = addr[i];
+			}
+			servAddress = new byte[]{(byte) 0, (byte) 0, (byte) 0, (byte) 0};
+			packetMessage[11] = (byte) desPri;
+			for(int j = 0; j < servAddress.length; j++){
+				packetMessage[j + 11] = servAddress[j];
+			}
+			DatagramPacket nData = new DatagramPacket(packetMessage, gatewayPort);
 			//receive ack from the IG for the connection being open
 			
-			
-			//build and send data packets with packetMessage and other arrays
-			
-			
-			// Message and its length		
-			String message = "Hello World!";
-			int lengthOfMessage = message.length(); 
-			byte[] data = new byte[lengthOfMessage];
-			data = message.getBytes();
+			while(open != false){
+				
 
-			// Create a datagram
-			DatagramPacket datagram = 
-				new DatagramPacket(data, lengthOfMessage, destination, serverPort);
+				//build and send data packets with packetMessage and other arrays
 
-			// Send a datagram carrying the message
-			clientSocket.send(datagram);
 
-			// Print out the message sent
-			System.out.println("Message sent is:   [" + message + "]");
+				// Message and its length		
+				String message = "Hello World!";
+				int lengthOfMessage = message.length(); 
+				byte[] data = new byte[lengthOfMessage];
+				data = message.getBytes();
 
-			// Prepare for receiving
-			// Create a buffer for receiving
-			byte[] receivedData = new byte[2048];
+				// Create a datagram
+				DatagramPacket datagram = 
+						new DatagramPacket(data, lengthOfMessage, destination, gatewayPort);
 
-			// Create a datagram
-			DatagramPacket receivedDatagram = 
-				new DatagramPacket(receivedData, receivedData.length);
+				// Send a datagram carrying the message
+				clientSocket.send(datagram);
 
-			// Receive a datagram
-			clientSocket.receive(receivedDatagram);
+				// Print out the message sent
+				System.out.println("Message sent is:   [" + message + "]");
 
-			// Display the message in the datagram
-			String echoMessage = new String(receivedData, 0, receivedDatagram.getLength());
-			System.out.println("Message echoed is: [" + echoMessage + "]");	
+				// Prepare for receiving
+				//should not have to change anything from here.
+				// Create a buffer for receiving
+				byte[] receivedData = new byte[2048];
+
+				// Create a datagram
+				DatagramPacket receivedDatagram = 
+						new DatagramPacket(receivedData, receivedData.length);
+
+				// Receive a datagram
+				clientSocket.receive(receivedDatagram);
+
+				// Display the message in the datagram
+				String echoMessage = new String(receivedData, 0, receivedDatagram.getLength());
+				System.out.println("Message echoed is: [" + echoMessage + "]");	
+			}
 		} 
 		catch (IOException ioEx) {
 			ioEx.printStackTrace();
@@ -107,5 +120,4 @@ public class Host {
 		}
 	
 	}
-
 }
