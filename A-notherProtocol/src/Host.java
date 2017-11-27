9e0de03e1d8c557c0a8954b02cdc918b6c211444
rs5634nr <rphillips2256@gmail.com>
2017-11-27 14:18:24 -0600

    here is a new one

--- A-notherProtocol/src/Host.java
+++ A-notherProtocol/src/Host.java
@@ -23,10 +23,12 @@ public class Host {
 		//used variable declaration
 		
 		String filename;
-		byte [] servAddress, checkArray, portArray;
+		byte [] servAddress, checkArray, portArray, gatePort;
 		byte [] addr;
-		byte [] message = new byte[1024];
-		byte [] header = new byte[20];
+		byte [] gatewayAddr = new byte[4];
+		byte [] message;
+		byte [] dataBuffer;
+		byte [] header = new byte[16];
 		int desPri, count, gatewayPort, serverPort, hostPort;
 		long check;
 		CRC32 checksum = new CRC32();
@@ -54,80 +56,144 @@ public class Host {
 			InetAddress destination = address;
 			
 			
-			// Determine server port number
+			// Determine server port number, gateway and host.
 			gatewayPort = 58989;
-			count = 0;
-			
-			
-			//Build the header and data of the open connection packet
-			
-			addr = new byte[] {(byte)146, (byte) 57, (byte) 194, (byte) 32};
-			
-			for(int i = 0; i < addr.length; i++){
-				message[i] = addr[i];
-				count++;
-			}
-			System.out.println(count);
+			serverPort = 18987;
 			hostPort = clientSocket.getLocalPort();
 			
+			//Prep the open connection message starting with prepping data for header and actual message.
+			//gateway ip address
+			for(int i = 0; i < b.length; i++) {
+				gatewayAddr[i] = b[i];
+			}
+			
+			addr = new byte[] {(byte)146, (byte) 57, (byte) 194, (byte) 32};
+			servAddress = new byte[] {(byte) 0, (byte) 0, (byte) 0, (byte) 0};
+			
+			//host port number
 			ByteBuffer portBuf = ByteBuffer.allocate(2);
 			portBuf.putShort((short) hostPort);
 			portArray = new byte[2];
-			
 			for(int i = 0; i<portArray.length; i++) {
 				portArray[i] = portBuf.get(i);
 			}
+			
+			//gateway port number
+			ByteBuffer gateBuf = ByteBuffer.allocate(2);
+			gateBuf.putShort((short) gatewayPort);
+			gatePort = new byte [2];
+			for(int i = 0; i < gatePort.length; i++) {
+				gatePort[i] = gateBuf.get(i);
+			}
+			
+			//server port number
+			ByteBuffer servPort = ByteBuffer.allocate(2);
+			servPort.putShort((short) serverPort);
+			byte [] servp = new byte [2];
+			for(int i = 0; i < servp.length; i++) {
+				servp[i] = servPort.get(i);
+			}
+
+			//Start to build the header.
+			//open header has fields for host ip, gateway ip, host port, gateway port, and checksum
+			//count is used to keep the data organized in the header.
+			count = 0;
+			//temp is to update count after first iteration.
 			int temp = 0;
-			for(int i = 0; i < portArray.length; i++){
-				message[i + count] = portArray[i];
+			// start with gateway address
+			for(int i = 0; i < gatewayAddr.length; i++) {
+				header[i] = gatewayAddr[i];
+				count++;
+			}
+			//debugging use
+			System.out.println(count);
+			//insert the port into the header.
+			for(int i = 0; i < gatePort.length; i++) {
+				header[i + count] = gatePort[i];
+				temp++;
+			}
+			
+			count += temp; // add the temp to the count
+			
+			System.out.println(count);//more debugging use.
+			
+			temp = 0; // reset temp
+			// put the host address in
+			for(int i = 0; i < addr.length; i++) {
+				header[i + count] = addr[i];
 				temp++;
 			}
 			
 			count += temp;
+			
 			System.out.println(count);
 			
+			temp = 0;
+			
+			//add in host port
+			for(int i = 0; i < portArray.length; i++) {
+				header[i + count] = portArray[i];
+				temp++;
+			}
+			count += temp;
+			System.out.println(count);
+			
+			temp = 0;
+			 // build the data portion of the message
+			
+			dataBuffer = new byte [7];
 			desPri = 1;
+			dataBuffer[0] = (byte) desPri;
 			
-			message[count + 1] = (byte) desPri;
-			count += 1;
-			System.out.println(count);
+			int newtemp = 0; 
 			
-			servAddress = new byte[] {(byte) 0, (byte) 0, (byte) 0, (byte) 0};
-			temp = 0;
-			for(int i = 0; i < servAddress.length; i++){
-				message[i + count] = servAddress[i];
-				temp++;
+			for(int i = 1; i < servAddress.length; i++) {
+				dataBuffer[i] = servAddress[i];
+				newtemp++;
 			}
 			
-			count += temp;
+			for(int i = 0; i < servp.length; i++) {
+				dataBuffer[i + newtemp] = servp[i];
+			}
 			
-			System.out.println(count);
-			
-			checksum.update(message);
-			
+			//checksum time
+			checksum.update(dataBuffer);
 			check = checksum.getValue();
-			
 			System.out.println(check);
-			
-			ByteBuffer checkSumBuf = ByteBuffer.allocate(4);
-			checkSumBuf.putInt((int) check);
+			ByteBuffer sum = ByteBuffer.allocate(4);
+			sum.putInt((int) check);
 			checkArray = new byte[4];
-			
-			for(int i = 0; i < 4; i++) {
-				checkArray[i] = checkSumBuf.get(i);
+			for(int i = 0; i < checkArray.length; i++) {
+				checkArray[i] = sum.get(i);
 			}
 			
+			//build the rest of the the header and build entire packet.
+			
 			temp = 0;
-			for(int i = 0; i < checkArray.length; i++){
-				message[i + count] = checkArray[i];
+			
+			for(int i = 0; i < checkArray.length; i++) {
+				header[i + count] = checkArray[i];
 				temp++;
 			}
 			
 			count += temp;
+			
 			System.out.println(count);
 			
-			DatagramPacket nData = new DatagramPacket(message, message.length);
-			clientSocket.send(nData);
+			message = new byte[23];
+			for(int i = 0; i < header.length; i++) {
+				message[i] = header[i];
+			}
+			
+			for(int i = 0; i < dataBuffer.length; i++) {
+				message[i + header.length] = dataBuffer[i];
+			}
+			//System.out.println(header.length);
+			//System.out.println(dataBuffer.length);
+			//System.out.println(message.length);
+			
+			//DatagramPacket nData = new DatagramPacket(message, message.length);
+			//clientSocket.send(nData);
 			//receive ack from the IG for the connection being open
 			
 			while(open != false){
