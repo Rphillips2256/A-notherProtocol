a86996ede0f4068976a4203b0afd383ea482fd70
rs5634nr <rphillips2256@gmail.com>
2017-12-07 11:49:03 -0600

    added more should be closer to complete on the client side.

--- A-notherProtocol/src/Host.java
+++ A-notherProtocol/src/Host.java
@@ -30,126 +30,144 @@ public class Host {
 		byte [] gatewayAddr = new byte[4];
 		byte [] message;
 		byte [] dataBuffer;
-		byte [] header = new byte[16];
-		byte [] mainDataMessage = new byte [2048];
-		int desPri = 0, gatewayPort, serverPort, hostPort;
+		byte [] header;
+		byte [] mainDataMessage;
+		int desPri = 0, gatewayPort, serverPort, hostPort, conID;
+		int packetCount = 0, resent = 0, seq = 0, len = 0;
 		long check;
 		CRC32 checksum = new CRC32();
 		boolean open = false;
-		boolean flag = true;
+		//boolean flag = true;
 		String name = "//Users/rs5644nr/Desktop/CS413/alice.txt";
-		File file = new File(name);
-		FileInputStream fis;
-		//BufferedReader buf;
 
 		//TODO: make a trace function.
-		while(flag) {
 
+
+		try {
+
+			// Open a UDP datagram socket
+			clientSocket = new DatagramSocket();
+
+			//Get the IG ip address and port number
+			//modify this to make it correct.
+			gatewayAddr = new byte[] {(byte) 146,(byte) 57,(byte) 194,(byte) 238};
+
+
+
+			InetAddress address = null;
 			try {
+				address = InetAddress.getByAddress(gatewayAddr);
+			} catch (UnknownHostException impossible) {
+				System.out.println("Unable to determine the host by address!");
+			}
+			InetAddress destination = address;
 
-				// Open a UDP datagram socket
-				clientSocket = new DatagramSocket();
 
-				//Get the IG ip address and port number
-				//modify this to make it correct.
-				gatewayAddr = new byte[] {(byte) 146,(byte) 57,(byte) 194,(byte) 238};
+
+			// Determine server port number, gateway and host.
+			//subject to change to an input type of method
+			gatewayPort = 58989;
+			serverPort = 18987;
+			hostPort = clientSocket.getLocalPort();
+
+			//Prep the open connection message starting with prepping data for header and actual message.
+			//gateway ip address
+
+
+			addr = new byte[] {(byte)146, (byte) 57, (byte) 194, (byte) 32};
+			servAddress = new byte[] {(byte) 0, (byte) 0, (byte) 0, (byte) 0};
+
+			header = openHead(addr, gatewayAddr, hostPort, gatewayPort);
+
+			dataBuffer = new byte[7];
+			dataBuffer = openMess(servAddress, serverPort, desPri);
+
+			//do the checksum should be done last after data has been built
+			checksum.update(dataBuffer);
+			check = checksum.getValue();
+			System.out.println(check);
+			ByteBuffer sum = ByteBuffer.allocate(4);
+			sum.putInt((int) check);
+			checkArray = new byte[4];
+			for(int i = 0; i < checkArray.length; i++) {
+				checkArray[i] = sum.get(i);
+			}
+
+			//build the rest of the the header and build entire packet.
+
+			int temp = 0;
+			int count1 = header.length - 4;
+			System.out.println(count1);
+
+			for(int i = 0; i < checkArray.length; i++) {
+				header[i + count1] = checkArray[i];
+				temp++;
+			}
+
+			count1 += temp;
+
+			System.out.println(count1);
+			//buid the openMessage
+			message = new byte[23];
+			for(int i = 0; i < header.length; i++) {
+				message[i] = header[i];
+			}
+
+			for(int i = 0; i < dataBuffer.length; i++) {
+				message[i + header.length] = dataBuffer[i];
+			}
+
+
+			//make a packet
+			DatagramPacket nData = new DatagramPacket(message, message.length, destination, gatewayPort);
+			//send the packet
+			clientSocket.send(nData);
+
+			//receive something from the IG for the connection being open
+
+			byte[] receivedData = new byte[2048];
+
+			// Create a datagram
+			DatagramPacket receivedDatagram = 
+					new DatagramPacket(receivedData, receivedData.length);
+
+			// Receive a datagram
+			clientSocket.receive(receivedDatagram);
+
+			//split data from header
+
+			byte [] receivedHeader = new byte[6];
+			byte[] recData = new byte[3];
+			int out = 0;
+			for(int i = 0; i < receivedHeader.length; i++) {
+				receivedHeader[i] = receivedData[i];
+			}
+			byte [] cSum = new byte [4];
+
+			for(int i = 0; i < cSum.length; i++) {
+				cSum[i] = receivedHeader[i + 4];
+			}
+
+			out += receivedHeader.length;
+
+			for(int i = 0; i < recData.length; i++) {
+				recData[i] = receivedData[i + out];
+			}
+
+			long sumCheck = toLong(cSum);
+
+			checksum.update(cSum);
+			long dataCheck = checksum.getValue();
+			byte [] gID = new byte[2];
+			for(int i = 0; i < gID.length; i++) {
+				gID[i] = receivedHeader[i];
+			}
+			
+			conID = getInt(gID);
+			
+			if(dataCheck == sumCheck) {
 				
 				
-				
-				InetAddress address = null;
-				try {
-					address = InetAddress.getByAddress(gatewayAddr);
-				} catch (UnknownHostException impossible) {
-					System.out.println("Unable to determine the host by address!");
-				}
-				InetAddress destination = address;
-				
-				
-				
-				// Determine server port number, gateway and host.
-				//subject to change to an input type of method
-				gatewayPort = 58989;
-				serverPort = 18987;
-				hostPort = clientSocket.getLocalPort();
-
-				//Prep the open connection message starting with prepping data for header and actual message.
-				//gateway ip address
-				
-
-				addr = new byte[] {(byte)146, (byte) 57, (byte) 194, (byte) 32};
-				servAddress = new byte[] {(byte) 0, (byte) 0, (byte) 0, (byte) 0};
-				
-				header = openHead(addr, gatewayAddr, hostPort, gatewayPort);
-				
-				dataBuffer = new byte[7];
-				dataBuffer = openMess(servAddress, serverPort, desPri);
-
-				//do the checksum should be done last after data has been built
-				checksum.update(dataBuffer);
-				check = checksum.getValue();
-				System.out.println(check);
-				ByteBuffer sum = ByteBuffer.allocate(4);
-				sum.putInt((int) check);
-				checkArray = new byte[4];
-				for(int i = 0; i < checkArray.length; i++) {
-					checkArray[i] = sum.get(i);
-				}
-
-				//build the rest of the the header and build entire packet.
-
-				int temp = 0;
-				int count1 = header.length;
-				System.out.println(count1);
-
-				for(int i = 0; i < checkArray.length; i++) {
-					header[i + count1] = checkArray[i];
-					temp++;
-				}
-
-				count1 += temp;
-
-				System.out.println(count1);
-				//buid the openMessage
-				message = new byte[23];
-				for(int i = 0; i < header.length; i++) {
-					message[i] = header[i];
-				}
-
-				for(int i = 0; i < dataBuffer.length; i++) {
-					message[i + header.length] = dataBuffer[i];
-				}
-
-				
-				//make a packet
-				DatagramPacket nData = new DatagramPacket(message, message.length, destination, gatewayPort);
-				//send the packet
-				clientSocket.send(nData);
-				
-				//receive something from the IG for the connection being open
-
-				byte[] receivedData = new byte[2048];
-
-				// Create a datagram
-				DatagramPacket receivedDatagram = 
-						new DatagramPacket(receivedData, receivedData.length);
-
-				// Receive a datagram
-				clientSocket.receive(receivedDatagram);
-
-				//Figure out where data message begins and header ends
-
-				byte [] receivedHeader = new byte[8];
-				byte[] recData = new byte[3];
-				int out = 0;
-				for(int i = 0; i < receivedHeader.length; i++) {
-					receivedHeader[i] = receivedData[i];
-				}
-				
-				out += receivedHeader.length;
-				
-				for(int i = 0; i < recData.length; i++) {
-					recData[i] = receivedData[i + out];
-				}
 				
 				String openMessage = new String(data(recData));
 				System.out.println(openMessage);
@@ -157,42 +175,91 @@ public class Host {
 				if(openMessage.toUpperCase() == "ACK") {
 					open = true;
 				}
+			} else {
+
+				clientSocket.send(nData);
+
+			}
+
+			while(open){
+				//prepare the packets for transmission,
+				createPackets(name);
 				
-				while(open){
-					//prepare the packets for transmission,
-					createPackets(name);
+				mainDataMessage = new byte[1512];
+				
+				byte [] m = packet.get(packetCount);
+				long dataSum = calculateChecksum(checksum, m);
+				len = (Files.readAllBytes(Paths.get(name)).length);
+				byte [] h = new byte[12];
+				
+				h = mainHeader(conID, seq, 1500, len, dataSum);
+				
+				int newCount = 0;
+				for(int i = 0; i < h.length; i++) {
 					
-					
-					
-					//TODO: helper methods????
-					
-					
-					
-					//set variables for the loop. and other needed parts.
-					
+					mainDataMessage[i] = h[i];
+					newCount++;
+				}
+				
+				for(int i = 0; i < m.length; i++) {
+					mainDataMessage[i + newCount] = m[i];
+				}
+				
+				DatagramPacket mainData = new DatagramPacket(mainDataMessage, mainDataMessage.length, destination, gatewayPort);
+				clientSocket.send(mainData);
+				clientSocket.setSoTimeout(1000);
+				
+				try {
+					clientSocket.receive(receivedDatagram);
+					receivedHeader = new byte[6];
+					recData = new byte[3];
+					out = 0;
+					for(int i = 0; i < receivedHeader.length; i++) {
+						receivedHeader[i] = receivedData[i];
+					}
+					cSum = new byte [4];
 
+					for(int i = 0; i < cSum.length; i++) {
+						cSum[i] = receivedHeader[i + 4];
+					}
 
-					/*
-					 *  TODO: Prepare for receiving
-					 *  should not have to change anything from here.
-					 *  split the packet into header and data also.
-					 *  Create a buffer for receiving
-					 */
+					out += receivedHeader.length;
+
+					for(int i = 0; i < recData.length; i++) {
+						recData[i] = receivedData[i + out];
+					}
+					
+					int curr = getInt(recData);
+					
+					if(curr == conID) {
+						packetCount++;
+						seq++;
+					} else {
+						clientSocket.send(mainData);
+						resent++;
+					}
+
+					
+				} catch (SocketTimeoutException e) {
+					System.out.println("Timeout reached: " + e);
+					
 				}
 				
 			}
 
-			catch (IOException ioEx) {
-				ioEx.printStackTrace();
-			} 
-			finally {
-				// Close the socket 
-				
-				clientSocket.close();
-			}
 		}
 
+		catch (IOException ioEx) {
+			ioEx.printStackTrace();
+		} 
+		finally {
+			// Close the socket 
+
+			clientSocket.close();
+		}
 	}
+
+	
 
 	public static StringBuilder data(byte [] a) {
 
@@ -307,7 +374,7 @@ public class Host {
 		return mess;
 	}
 	
-	public static byte [] mainHeader(int id, int seq, int dataLen, int totalSize) {
+	public static byte [] mainHeader(int id, int seq, int dataLen, int totalSize, long check) {
 		byte [] head = new byte[12];
 		int count = 0;
 		int temp = 0;
@@ -347,7 +414,25 @@ public class Host {
 			temp++;
 		}
 		
+		count += temp;
+		temp = 0;
+		
+		ByteBuffer cs = ByteBuffer.allocate(4);
+		cs.putInt((int) check);
+		for(int i = 0; i < 4; i++) {
+			head[i+count] = cs.get(i);
+		}
+		
 		return head;
+	}
+	
+	public static long calculateChecksum(CRC32 c,byte [] d ) {
+		long s = 0;
+		
+		c.update(d);
+		s = c.getValue();
+		
+		return s;
 	}
 	
 	public static List createPackets(String file) throws IOException{
@@ -355,6 +440,7 @@ public class Host {
 	     //read the file into array
 	        Path path = Paths.get(file);
 	        byte[] data = Files.readAllBytes(path);
+	        final int length = data.length;
 
 	        int position = 0;
 	        int messageSent = data.length;
@@ -380,14 +466,24 @@ public class Host {
 	        
 	        //increase the position by max data size bytes
 	        position = position + 1500;
-	        //decrease the postion by 16 bytes
+	        //decrease the position by 16 bytes
 	        messageSent = messageSent - 1500;
 
 	        
-	        }//end Whileloop
+	        }//end
 	   
 	        return packet; 
-	}       
+	} 
+	
+	public static int getInt(byte [] h) {
+		int id = 0;
+		
+		for(int i = 0; i < 2; i++) {
+			id = ( (id << 8) + ( h[i] & 0xff ) );
+		}
+		
+		return id;
+	}
 	
 	//TODO: trace implentation menu screen????????
 	//TODO: other menus as needed??!?!?!?!?
